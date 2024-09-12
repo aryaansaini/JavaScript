4d2228d5c19850375efb096b6eab540a56c64c06
aryaansaini <141976345+aryaansaini@users.noreply.github.com>
2024-09-12 18:31:58 +0000

    conversion of datatype

---
+++ 01_basic/01_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all js code as newer version
+
+// alert("hello") //we are using nodejs, not browser
+
+console.log(3
+    +3) //code readability should be high
+ console.log("aryan")
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object 
+
+console.log(typeof "aryan");
+console.log(typeof null)  // object
+console.log(typeof undefined)  //undefind
+
