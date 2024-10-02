80a601d913190a65b8b0269cc82b18de660d7ef1
aryaansaini <141976345+aryaansaini@users.noreply.github.com>
2024-10-02 16:08:29 +0000

    Array2

---
+++ 02_basic/02_arrays.js
@@ -0,0 +1,36 @@
+const marrval_heros = ["thor", "Ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+// marrval_heros.push(dc_heros)
+// console.log(marrval_heros[3][1]);
+
+
+// const allHeros  = marrval_heros.concat(dc_heros)
+// console.log(marrval_heros);
+
+
+
+const all_new_heros  = [...marrval_heros, ...dc_heros]
+console.log(all_new_heros);
+
+const anothr_array  = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+const real_another_array  = anothr_array.flat(Infinity)
+
+console.log(anothr_array);
+console.log(real_another_array);
+
+
+console.log(Array.isArray("Aryan"))
+console.log(Array.from("Aryan"));
+
+
+console.log(Array.from({name: "Aryan"})); //interesting
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1,score2,score3));
+
+
+
